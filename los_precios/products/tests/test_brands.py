01267be907fa7e4cc7b8b33deaee5b3438c84c22
Juan Sebastian Avila <sebas.aviloncho@gmail.com>
2020-08-07 10:29:05 -0500

    Create BrandAPITestCase

--- los_precios/products/tests/test_brands.py
+++ los_precios/products/tests/test_brands.py
@@ -3,6 +3,10 @@
 #Django
 from django.test import TestCase
 
+#Django REST Framework
+from rest_framework.test import APITestCase
+from rest_framework import status
+
 #Model
 from los_precios.products.models import Brand
 from los_precios.users.models import User
@@ -26,3 +30,19 @@ class BrandManagerTestCase(TestCase):
         )
 
         self.assertIsNotNone(brand)
+    
+class BrandAPITestCase(APITestCase):
+
+    def setUp(self):
+        """Test case setup"""
+        self.user = User.objects.create(
+            name='Juan Avila',
+            email='juanavila@mail.com',
+            username='juanavila',
+            password='admin123'
+        )
+    
+    def test_response_success(self):
+        url = '/products/brands/'
+        request = self.client.get(url)
+        self.assertEqual(request.status_code, status.HTTP_200_OK)
